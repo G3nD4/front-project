c300ace5d9d342c283e0324e274464de8f23c3d4
Draxster <118171977+Draxster@users.noreply.github.com>
2023-07-10 02:26:15 +0300

    Add About page!

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import "/src/routes/about/styles/about.scss";
+</script>
+
 <div class="header">
     <div class="container">
         <div class="header-title">
@@ -25,3 +29,100 @@
         </nav>
     </div>
 </div>
+
+<div class="our-company">
+    <div class="container">
+        <div class="company">
+            <h1 class="intro_company">Our Company</h1>
+            <img alt="Starbucks storefront" src="/src/img/StarBucks.jpg">
+        </div>
+    </div>
+</div>
+            
+<div class="our-heritage">
+    <div class="container">
+        <h1>Our Heritage</h1>
+        <div class="info">
+            <p>Our story begins in 1971 along the cobblestone streets of Seattle’s historic Pike Place Market. It was here where Starbucks opened its first store, offering fresh-roasted coffee beans, tea and spices from around the world for our customers to take home. Our name was inspired by the classic tale, “Moby-Dick,” evoking the seafaring tradition of the early coffee traders.</p>
+            <p>Ten years later, a young New Yorker named Howard Schultz would walk through these doors and become captivated with Starbucks coffee from his first sip. After joining the company in 1982, a different cobblestone road would lead him to another discovery. It was on a trip to Milan in 1983 that Howard first experienced Italy’s coffeehouses, and he returned to Seattle inspired to bring the warmth and artistry of its coffee culture to Starbucks. By 1987, we swapped our brown aprons for green ones and embarked on our next chapter as a coffeehouse.</p>
+            <p>Starbucks would soon expand to Chicago and Vancouver, Canada and then on to California, Washington, D.C. and New York. By 1996, we would cross the Pacific to open our first store in Japan, followed by Europe in 1998 and China in 1999. Over the next two decades, we would grow to welcome millions of customers each week and become a part of the fabric of tens of thousands of neighborhoods all around the world. In everything we do, we are always dedicated to Our Mission: With every cup, with every conversation, with every community - we nurture the limitless possibilities of human connection.</p>
+        </div>
+    </div>
+</div>  
+
+<div class="coffee-craft">
+    <div class="container">
+        <div class="info">
+            <h1>Coffee & Craft</h1>
+            <img alt="Starbucks barista" src="/src/img/Barista.jpg"/>
+            <div class="craft-info">
+                <p>It takes many hands to craft the perfect cup of coffee – from the farmers who tend to the red-ripe coffee cherries, to the master roasters who coax the best from every bean, and to the barista who serves it with care. We are committed to the highest standards of quality and service, embracing our heritage while innovating to create new experiences to savor.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="our-partners">
+    <div class="container">
+        <div class="info">
+            <h1>Our Partners</h1>
+            <img alt="Starbucks barista" src="/src/img/Partners.jpg"/>
+            <div class="partners-info">
+                <p>We like to say that we are not in the coffee business serving people, but in the people business serving coffee. Our employees – who we call partners – are at the heart of the Starbucks experience. We are committed to making our partners proud and investing in their health, well-being and success and to creating a culture of belonging where everyone is welcome.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="believe">
+    <div class="container">
+        <div class="info">
+            <h1>We Believe in the Pursuit of Doing Good</h1>
+            <img alt="Starbucks barista" src="/src/img/Views.jpg"/>
+            <div class="believe-info">
+                <p>As it has been from the beginning, our purpose goes far beyond profit. We believe Starbucks can, and should, have a positive impact on the communities we serve.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="people">
+    <div class="container">
+        <div class="info">
+            <h1>People</h1>
+            <div class="people-info">
+                <p>Our aspiration is to be people positive – investing in humanity and the well-being of everyone we connect with, from our partners to coffee farmers to the customers in our stores and beyond.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="planet">
+    <div class="container">
+        <div class="info">
+            <h1>Planet</h1>
+            <div class="planet-info">
+                <p>We are striving to become resource positive – giving back more than we take from the planet. We are working to store more carbon than we emit, replenish more freshwater than we use, and eliminate waste. We know we can’t do it alone. It takes all of us.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="learn">
+    <div class="container">
+        <div class="learn-info">
+            <h2>Learn more about us</h2>
+        </div>
+    </div>
+</div>
+
+<div class="stories">
+    <div class="container">
+        <div class="info">
+            <h1>Stories & News</h1>
+            <div class="stories-info">
+                <p>Behind every cup of coffee is a story. Enjoy some of our favorites along with all the latest news from the storytellers at Starbucks Stories.</p>
+            </div>
+        </div>
+    </div>
+</div> -->
