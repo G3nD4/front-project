46f6de191cec21d81a72f302cd007392d53149a8
G3nD4 <sagalievadel0@gmail.com>
2023-07-22 17:36:32 +0300

    installed firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdAS56wt9Tq8AExIMfwfVD7LI3chQtu8U",
+  authDomain: "front-project-133bc.firebaseapp.com",
+  databaseURL: "https://front-project-133bc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "front-project-133bc",
+  storageBucket: "front-project-133bc.appspot.com",
+  messagingSenderId: "242911610176",
+  appId: "1:242911610176:web:97ac668cff28be7263a6a5",
+  measurementId: "G-S37TK8Y2QQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function addToCart
